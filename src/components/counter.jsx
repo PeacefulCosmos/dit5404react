b896602797d3a64c474d36dae24dcb543a972634
Roy Chan <cosmosroy0207@gmail.com>
2021-04-19 01:28:15 +0800

    add sematic ui

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 
-class Counter extends Component {
-  state = {};
-  render() {
-    return <h1>Hello World</h1>;
-  }
-}
+export const Counter = () => {
+  const [number, setNumber] = React.useState(0);
 
-export default Counter;
+  const incre = () => {
+    setNumber(number + 1);
+  };
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <Button animated="vertical">
+        <Button.Content hidden>Shop</Button.Content>
+        <Button.Content visible>
+          <Icon name="shop" />
+        </Button.Content>
+      </Button>
+      <Button
+        content="Click Here"
+        onClick={() => incre()}
+        color="google plus"
+      />
+    </div>
+  );
+};
