c362f161f43b9d5627702bac4dd4811ba3d455d1
roy chan <roy.chan@inreality.com>
2021-04-22 18:14:50 +0800

    trailer

--- src/components/movie/TrailerSlide.jsx
+++ src/components/movie/TrailerSlide.jsx
@@ -5,29 +5,29 @@ import {
   ButtonBack,
   ButtonNext,
 } from "pure-react-carousel";
-import { Container } from "semantic-ui-react";
+import { Button, Container } from "semantic-ui-react";
 import { Trailer } from "./Trailer";
 export const TrailerSlide = ({ movies }) => {
   console.log(movies[0].movie_id);
   return (
-    <Container>
-      <CarouselProvider
-        className="carousel-size"
-        visibleSlides={1}
-        naturalSlideWidth={100}
-        naturalSlideHeight={250}
-        totalSlides={movies.length}
-      >
-        <ButtonBack>Back</ButtonBack>
-        <Slider>
-          {movies.map((movie) => (
-            <Slide index={movie.movie_id}>
-              <Trailer movie={movie} />
-            </Slide>
-          ))}
-        </Slider>
-        <ButtonNext>Next</ButtonNext>
-      </CarouselProvider>
-    </Container>
+    // <Container>
+    <CarouselProvider
+      className="carousel-size slider"
+      visibleSlides={1}
+      naturalSlideWidth={500}
+      naturalSlideHeight={250}
+      totalSlides={movies.length}
+    >
+      <ButtonBack className="buttonBack">Back</ButtonBack>
+      <Slider>
+        {movies.map((movie) => (
+          <Slide index={movie.movie_id}>
+            <Trailer movie={movie} />
+          </Slide>
+        ))}
+      </Slider>
+      <ButtonNext className="buttonNext">Next</ButtonNext>
+    </CarouselProvider>
+    // </Container>
   );
 };
